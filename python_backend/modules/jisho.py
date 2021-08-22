0ca3e3e81298f5787107d82352b686eafd8ffe5f
Ben Leong <benleong0@gmail.com>
2021-08-22 17:13:33 +0100

    typing

--- python_backend/modules/jisho.py
+++ python_backend/modules/jisho.py
@@ -1,5 +1,5 @@
-from typing import List
+from typing import Dict, List
 
 
-def get_vocab_data(word_list: List[str]):
+def get_vocab_data(word_list: List[str]) -> Dict[str, List[Dict]]:
     return {key:[] for key in word_list}
--- python_backend/modules/wadoku.py
+++ python_backend/modules/wadoku.py
@@ -1,5 +1,5 @@
-from typing import List
+from typing import Dict, List
 
 
-def get_accent_dict(word_list: List[str]):
+def get_accent_dict(word_list: List[str]) -> Dict[str, List[str]]:
     return {key:[] for key in word_list}
