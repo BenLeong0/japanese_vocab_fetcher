670a9476a25aaed7d1afd254b7424f8a452aadd9
benleong0 <benleong@urbanjungle.com>
2021-08-22 14:07:37 +0100

    dict

--- python_backend/coordinator.py
+++ python_backend/coordinator.py
@@ -1,22 +1,23 @@
-
-
-
+from collections import defaultdict
+from threading import Thread
 from typing import Dict, List
 
 from modules import ojad, suzuki, wadoku
 
 
 def get_info(word_list: List[str]) -> Dict:
+    results_dict = defaultdict(dict)
+
     # multi thread!
-    ojad_dict = ojad.get_accent_dict(word_list)
-    suzuki_dict = suzuki.get_accent_dict(word_list)
-    wadoku_dict = wadoku.get_accent_dict(word_list)
+    results_dict['ojad_dict'] = ojad.get_accent_dict(word_list)
+    results_dict['suzuki_dict'] = suzuki.get_accent_dict(word_list)
+    results_dict['wadoku_dict'] = wadoku.get_accent_dict(word_list)
 
     return generate_response(
         word_list=word_list,
-        ojad_dict=ojad_dict,
-        suzuki_dict=suzuki_dict,
-        wadoku_dict=wadoku_dict
+        ojad_dict=results_dict['ojad_dict'],
+        suzuki_dict=results_dict['suzuki_dict'],
+        wadoku_dict=results_dict['wadoku_dict']
     )
 
 
