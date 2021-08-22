6679d5697120c9d4e9d981f866c1ef7656a3d8fc
Ben Leong <benleong0@gmail.com>
2021-08-22 23:01:16 +0100

    remove punctuation util

--- python_backend/utils.py
+++ python_backend/utils.py
@@ -1,4 +1,5 @@
 import json
+import re
 from typing import Any
 
 from flask.wrappers import Request, Response
@@ -15,3 +16,7 @@ def create_successful_response(payload: Any):
 def create_failed_response(payload: Any):
     resp = json.dumps(payload)
     return Response(resp, status=400)
+
+
+def remove_punct(self, s: str) -> str:
+    return re.sub('[\￨･~]', '', s)
