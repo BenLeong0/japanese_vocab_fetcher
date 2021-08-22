e698ff3fea16e491b97d8331c9464b1cc9225af4
benleong0 <benleong@urbanjungle.com>
2021-08-22 14:39:44 +0100

    multithreading

--- python_backend/coordinator.py
+++ python_backend/coordinator.py
@@ -1,23 +1,43 @@
+from enum import Enum
 from collections import defaultdict
 from threading import Thread
 from typing import Dict, List
 
 from modules import ojad, suzuki, wadoku
 
+class Modules(Enum):
+    OJAD = ojad
+    SUZUKI = suzuki
+    WADOKU = wadoku
+
+
+class ModuleError(Exception):
+    pass
+
 
 def get_info(word_list: List[str]) -> Dict:
     results_dict = defaultdict(dict)
 
-    # multi thread!
-    results_dict['ojad_dict'] = ojad.get_accent_dict(word_list)
-    results_dict['suzuki_dict'] = suzuki.get_accent_dict(word_list)
-    results_dict['wadoku_dict'] = wadoku.get_accent_dict(word_list)
+    def call_script(src: Modules):
+        if src not in Modules:
+            raise ModuleError(f"{src} is not a valid module")
+        results_dict[src] = src.value.get_accent_dict(word_list)
+
+
+    threads = [Thread(target=call_script, args=[module]) for module in Modules]
+    print(threads)
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+    print(results_dict)
 
     return generate_response(
         word_list=word_list,
-        ojad_dict=results_dict['ojad_dict'],
-        suzuki_dict=results_dict['suzuki_dict'],
-        wadoku_dict=results_dict['wadoku_dict']
+        ojad_dict=results_dict[Modules.OJAD],
+        suzuki_dict=results_dict[Modules.SUZUKI],
+        wadoku_dict=results_dict[Modules.WADOKU]
     )
 
 
