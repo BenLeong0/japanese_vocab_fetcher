fee6c1d1f5dc4282811eb1e43fec30bced7384c2
benleong0 <benleong@urbanjungle.com>
2021-08-22 13:30:13 +0100

    successful_response tuil

--- python_backend/utils.py
+++ python_backend/utils.py
@@ -1,7 +1,13 @@
 import json
+from typing import Any
 
-from flask.wrappers import Request
+from flask.wrappers import Request, Response
 
 
 def get_words_from_request(request: Request):
     return json.loads(dict(request.args)['words'])
+
+
+def successful_response(payload: Any):
+    resp = json.dumps(payload)
+    return Response(resp, status=200)
