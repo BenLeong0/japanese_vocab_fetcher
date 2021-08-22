59be83094c67fc38781b713c066a63d028fffb6a
benleong0 <benleong@urbanjungle.com>
2021-08-22 13:32:40 +0100

    get query strings

--- python_backend/app.py
+++ python_backend/app.py
@@ -1,7 +1,10 @@
 import json
 
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask.wrappers import Response
 from flask_cors import CORS
+
+import utils
 
 app = Flask("app")
 CORS(app)
@@ -9,7 +12,8 @@ CORS(app)
 
 @app.route('/words')
 def homepage():
-    return json.dumps("['yo']")
+    print(utils.get_words_from_request(request))
+    return utils.response_successful(['yo'])
 
 
 if __name__ == "__main__":
