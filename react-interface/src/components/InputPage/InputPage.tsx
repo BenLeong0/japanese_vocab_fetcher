d4d1598de17d8f04768ba57b3e0222c926f0b772
= <benleong0@gmail.com>
2021-08-22 01:08:52 +0100

    inputpage

--- react-interface/src/components/InputPage/InputPage.tsx
+++ react-interface/src/components/InputPage/InputPage.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import HttpService from '../core/HttpService';
 
 import './InputPage.css';
 
@@ -7,12 +8,32 @@ export interface InputPageProps {
 }
 
 const InputPage: React.FC<InputPageProps> = () => {
+    const httpService = new HttpService();
+    const [text, setText] = useState<string>('');
+
+    const getWords = (): string[] => {
+        return text.split('\n').filter(word => word.trim().length > 0);
+    }
+
+    const sendWords = async (event: React.FormEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+
+        const words: string[] = getWords();
+        const wordsString: string = JSON.stringify(words);
+
+        const queryParams = {
+            words: wordsString,
+        };
+        httpService.makeGetRequest('', queryParams);
+    }
+
     return (
         <div className="input-page">
-            <form>
-                <textarea />
-                <button type="submit">Submit</button>
-            </form>
+            <textarea
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+            <button type="submit" onClick={sendWords}>Submit</button>
         </div>
     );
 }
