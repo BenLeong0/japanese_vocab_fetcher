0335217f8b5854df7206fce0bdddac4c301fa6d8
Ben Leong <benleong0@gmail.com>
2021-08-24 23:33:36 +0100

    test resources

---
+++ python_backend/testing/test_wadoku/example_responses.py
@@ -0,0 +1,34 @@
+from typing import List, TypedDict
+
+def get_file_as_string(filename):
+    filelocation = "testing/test_wadoku/"
+    with open(filelocation+filename, 'r', encoding='utf8') as myfile:
+        return myfile.read()
+
+
+class TestingDict(TypedDict):
+    resp: str
+    expected_output: List[str]
+
+
+WADOKU_MEGANE: TestingDict = {
+    'input': ['眼鏡'],
+    'resp': get_file_as_string('resp_megane.html'),
+    'expected_output': {
+        '眼鏡': ["め' がね", "がんきょう" ],
+        '眼鏡屋': [],
+        '眼鏡橋': ["めがね' ばし"]
+    },
+}
+
+
+WADOKU_COMEBACK: TestingDict = {
+    'input': ['カムバック'],
+    'resp': get_file_as_string('resp_comeback.html'),
+    'expected_output': {
+        'カムバック': ["かむば' っく", "かむばっく" ],
+        'カムバックする': ["かむば' っくする"],
+    },
+}
+
+print(WADOKU_MEGANE['resp'])
