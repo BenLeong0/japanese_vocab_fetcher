8a8e35de0ec97567398e63836558be973f2dd2c1
Ben Leong <benleong0@gmail.com>
2021-08-26 15:11:57 +0100

    test get url

--- python_backend/testing/test_wadoku/test_wadoku.py
+++ python_backend/testing/test_wadoku/test_wadoku.py
@@ -1,3 +1,5 @@
+# pylint: disable=line-too-long
+
 from collections import defaultdict
 import pytest
 from requests.models import Response
@@ -6,15 +8,27 @@ from modules import wadoku
 
 from testing.test_wadoku.example_responses import (
     WADOKU_MEGANE,
-    WADOKU_COMEBACK
+    WADOKU_COMEBACK,
+    WADOKU_TABERU_GAKUSEI
 )
 
 class FakeResponse:
     def __init__(self, text):
         self.text = text
 
-@pytest.mark.parametrize("test_dict", [WADOKU_MEGANE, WADOKU_COMEBACK])
-def test_get_accent_dict(monkeypatch, test_dict):
-    monkeypatch.setattr("requests.post", lambda x, timeout: FakeResponse(test_dict['resp']))
-    print(wadoku.get_accent_dict(test_dict['input']))
-    assert False
+
+@pytest.mark.parametrize(
+    "word_list, expected_url",
+    [
+        [['眼鏡'], "https://www.wadoku.de/search/%E7%9C%BC%E9%8F%A1"],
+        [['食べる', '学生'], "https://www.wadoku.de/search/%E9%A3%9F%E3%81%B9%E3%82%8B%20%E5%AD%A6%E7%94%9F"]
+    ]
+)
+def test_get_url(word_list, expected_url):
+    assert wadoku.get_url(word_list) == expected_url
+
+# @pytest.mark.parametrize("test_dict", [WADOKU_MEGANE, WADOKU_COMEBACK, WADOKU_TABERU_GAKUSEI])
+# def test_get_accent_dict(monkeypatch, test_dict):
+#     monkeypatch.setattr("requests.post", lambda x, timeout: FakeResponse(test_dict['html']))
+#     print(wadoku.get_accent_dict(test_dict['input']))
+#     assert False
