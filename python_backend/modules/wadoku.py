dafb288b7bcce97bd9c8b1b261eb707b4cd811f9
Ben Leong <benleong0@gmail.com>
2021-08-26 18:07:52 +0100

    build dict

--- python_backend/modules/wadoku.py
+++ python_backend/modules/wadoku.py
@@ -1,8 +1,8 @@
 from collections import defaultdict
-from typing import DefaultDict, Dict, List, Set, Tuple
+from typing import DefaultDict, Dict, List, Set, Tuple, final
+import urllib
 
 import requests
-import urllib
 
 from bs4 import BeautifulSoup as Soup
 from bs4.element import PageElement
@@ -14,14 +14,12 @@ def get_accent_dict(word_list: List[str]) -> Dict[str, List[str]]:
     if not word_list:
         return {}
 
-    accent_dict = defaultdict([])
 
     html = get_html(word_list)
     word_sections = get_sections(html)
-    extracted_writings = extract_writings(word_sections)
-    # accent_dict = build_accent_dict(html_sections, single=single)
-    # return accent_dict
-    # return {key:accent_dict[key] for key in word_list}
+    accent_dict = build_accent_dict(word_sections)
+
+    return {word:accent_dict[word] for word in word_list}
 
 
 # Get HTML
@@ -50,124 +48,47 @@ def get_sections(html: Soup) -> List[Tuple[Soup, List[Soup]]]:
     ]
 
 
-def extract_writings(word_sections: List[Tuple[Soup, List[Soup]]]) -> List[Tuple[str, List[Soup]]]:
-    result = []
-    for word_section in word_sections:
-        result += extract_writing(word_section)
-    return result
+def extract_writings(writing_html: Soup) -> List[str]:
+    writings = writing_html.text.split('；')
+    return [remove_punct(writing) for writing in writings]
 
 
-def extract_writing(word_section: Tuple[Soup, List[Soup]]) -> List[Tuple[str, List[Soup]]]:
-    writings = word_section[0].text.split('；')
-    return [(remove_punct(writing), word_section[1]) for writing in writings]
+def extract_reading(reading_html: Soup) -> str:
+    spans: List[Soup] = [span for span in reading_html.findChild().findAll('span') if span != '…']
+
+    if not spans:
+        return ''
+
+    # Initialise with first char
+    curr = remove_punct(spans[0].text)
+    height = 1 if 't' in spans[0]['class'] else 0
+
+    # Iterate over alternating heights
+    for (i, span) in enumerate(spans[1:], start=1):
+        if 't' in span['class'] and height == 0:
+            if i != 1:
+                curr += "* "
+            height = 1
+        elif 'b' in span['class'] and height == 1:
+            curr += "' "
+            height = 0
+
+        curr += remove_punct(span.text)
+
+    # Final drop if 尾高
+    if 'r' in spans[-1]['class'] and height == 1:
+        curr += "'"
+
+    return curr
 
 
-def extract_sections(soup: Soup, single: bool = False) -> List[PageElement]:
-    table_rows = [Soup(str(x), 'html.parser') for x in soup.findAll('tr')]
-    if single:
-        return table_rows
-
-    sections: List[PageElement] = []
-    for row in table_rows:
-        if not row.has_attr('class'):
-            sections.append(row)
-        else:
-            sections[-1].append(row)
-
-    return [Soup(str(x)) for x in sections]
-
-
-# Build accent dict mult
-
-def build_accent_dict(
-    html_sections: List[Soup],
-    single: bool = False,
-) -> DefaultDict[str, List[str]]:
-    if single:
-        extract_subsections = extract_subsections_single
-        extract_kakikata = extract_kakikata_single
-        extract_yomikata = extract_yomikata_single
-    else:
-        extract_subsections = extract_subsections_mult
-        extract_kakikata = extract_kakikata_multiple
-        extract_yomikata = extract_yomikata_multiple
-
+def build_accent_dict(word_sections: List[Tuple[Soup, List[Soup]]]) -> DefaultDict:
     accent_dict = defaultdict(list)
-    for section in html_sections:
-        writing_sections, reading_sections = extract_subsections(section)
-        writing = extract_kakikata(writing_sections)
-        accents = extract_yomikata(reading_sections)
-        accent_dict[writing] = accents
+
+    for writing_html, reading_htmls in word_sections:
+        writings = extract_writings(writing_html)
+        readings = [extract_reading(reading_html) for reading_html in reading_htmls]
+        for writing in writings:
+            accent_dict[writing] += readings
 
     return accent_dict
-
-
-def extract_kakikata_single(section: Soup) -> str:
-    midashi_section: Soup = section.find('th', class_='focalPhrase')
-    if midashi_section is None:
-        return ''
-    return midashi_section.text.strip()
-
-
-def extract_kakikata_multiple(section: Soup) -> str:
-    midashi_section: Soup = section.find('th', class_='focalPhrase')
-    if midashi_section is None:
-        return ''
-    return midashi_section.text.strip()
-
-
-def extract_yomikata_single(section: List[Soup]) -> List[str]:
-    print([reading.text.strip() for reading in section])
-    return [reading.text.strip() for reading in section]
-
-
-def extract_yomikata_multiple(section: Soup) -> List[str]:
-    # TODO: Check if NO ACCENT!
-    accent_sections: List[Soup] = section.find_all('span', class_='accent')
-
-    accents: Set[str] = set()
-    for accent in accent_sections:
-        spans: List[Soup] = [span for span in accent.children if span != '…']
-
-        # Initialise with first char
-        curr = remove_punct(spans[0].text)
-        if 't' in spans[0]['class']:
-            if 'r' in spans[0]:
-                curr += "' "
-                H = 0
-            H = 1
-        else: H = 0
-
-        # Iterate over alternating heights
-        for (i, span) in enumerate(spans[1:], start=1):
-            if 't' in span['class'] and H == 0:
-                if i != 1: curr += "* "
-                H = 1
-            elif 'b' in span['class'] and H == 1:
-                curr += "' "
-                H = 0
-
-            curr += remove_punct(span.text)
-
-        # Final drop if 尾高
-        if 'r' in spans[-1]['class'] and H == 1:
-            curr += "'"
-
-        accents.add(curr)
-
-    return list(accents)
-
-
-# Build accent dict single
-
-
-def extract_subsections_single(section: Soup) -> Tuple[Soup, List[Soup]]:
-    """Returns a tuple (writing_section, reading_sections)"""
-    writing_sections = section.find('div', class_='japanese')
-    reading_sections = section.findAll('span', class_='accent')
-    return writing_sections, reading_sections
-
-def extract_subsections_mult(section: Soup) -> Tuple[Soup, List[Soup]]:
-    """Returns a tuple (writing_section, reading_sections)"""
-    writing_sections = section.find('span', class_='accent')
-    return writing_sections, writing_sections
