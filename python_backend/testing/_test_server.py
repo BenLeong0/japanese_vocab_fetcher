424b75b8a3f67511df782e96569cdab533993f24
benleong0 <benleong@urbanjungle.com>
2021-09-06 16:18:26 +0100

    test_server

--- python_backend/testing/_test_server.py
+++ python_backend/testing/_test_server.py
@@ -3,17 +3,18 @@ import json
 import pytest
 import requests
 
+from testing.dicts import TEST_DICTS, TEST_DICT_IDS
+
+
+@pytest.fixture(params=TEST_DICTS, ids=TEST_DICT_IDS)
+def test_dict(request):
+    return request.param
+
 API_URL = "http://127.0.0.1:5000/words"
 
-@pytest.mark.parametrize(
-    "word_list",
-    [
-        ['行く'],
-        ['行く', '眼鏡'],
-        [],
-    ]
-)
-def test_words_endpoint(word_list):
+
+def test_words_endpoint(test_dict):
+    word_list = test_dict['input']
     encoded_word_list = json.dumps(word_list)
     payload = {'words': encoded_word_list}
     result = requests.get(API_URL, params=payload)
