c0481d2ddba4704879f4c20f4932a483469bdda6
benleong0 <benleong@urbanjungle.com>
2021-09-06 16:21:20 +0100

    comment

--- python_backend/testing/test_wadoku.py
+++ python_backend/testing/test_wadoku.py
@@ -7,6 +7,7 @@ from modules import wadoku
 from testing.dicts import TEST_DICTS, TEST_DICT_IDS
 
 
+# For each test, try with every dict in TEST_DICTS
 @pytest.fixture(params=TEST_DICTS, ids=TEST_DICT_IDS)
 def test_dict(request):
     return request.param
