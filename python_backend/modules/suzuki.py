6582ce4e1f904c2ea7dba980d44cb402bac39d19
Ben Leong <benleong0@gmail.com>
2021-09-06 21:58:47 +0100

    html

--- python_backend/modules/suzuki.py
+++ python_backend/modules/suzuki.py
@@ -1,5 +1,39 @@
 from typing import Dict, List
 
+from bs4 import BeautifulSoup as Soup
+import requests
+
 
 def get_accent_dict(word_list: List[str]) -> Dict[str, List[str]]:
-    return {key:[] for key in word_list}
+    if not word_list:
+        return {}
+
+    html = get_html(word_list)
+    # word_sections = get_sections(html)
+    # accent_dict = build_accent_dict(word_sections)
+
+    return {word:[] for word in word_list}
+
+
+# Get HTML
+
+def get_formdata(word_list: List[str]) -> Dict[str, str]:
+    formdata = {
+        "data[Phrasing][curve]": "advanced",
+        "data[Phrasing][accent]": "advanced",
+        "data[Phrasing][accent_mark]": "all",
+        "data[Phrasing][estimation]": "crf",
+        "data[Phrasing][analyze]": "true",
+        "data[Phrasing][phrase_component]": "invisible",
+        "data[Phrasing][param]": "invisible",
+        "data[Phrasing][subscript]": "visible",
+    }
+    formdata["data[Phrasing][text]"] = "\n".join(word_list)
+    return formdata
+
+
+def get_html(word_list: List[str]) -> Soup:
+    url = 'http://www.gavo.t.u-tokyo.ac.jp/ojad/phrasing/index'
+    formdata = get_formdata(word_list)
+    html = requests.post(url, formdata, timeout=20).text
+    return Soup(html, 'html.parser')
