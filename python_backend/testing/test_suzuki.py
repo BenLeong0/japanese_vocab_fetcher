0d7cf6133f3915b1e50be24342917fb04fc6fc27
benleong0 <benleong@urbanjungle.com>
2021-09-12 10:47:32 +0100

    precopy and adapt tests

--- python_backend/testing/test_suzuki.py
+++ python_backend/testing/test_suzuki.py
@@ -42,3 +42,52 @@ def test_get_sections(test_dict: TestDict):
         (section['writing_section'], section['reading_section'], section['accent_section'])
         for section in expected_sections
     ]
+
+
+# def test_extract_writings(test_dict: TestDict):
+#     """
+#     - GIVEN an html sections
+#     - WHEN the writing is extracted
+#     - THEN check all the correct writings are extracted
+#     """
+#     for section in test_dict['suzuki']['expected_sections']:
+#         assert suzuki.extract_writings(section['writing_section']) == section['writings']
+
+
+# def test_extract_readings(test_dict: TestDict):
+#     """
+#     - GIVEN an html sections
+#     - WHEN the writing is extracted
+#     - THEN check all the correct writings are extracted
+#     """
+#     for section in test_dict['suzuki']['expected_sections']:
+#         for html_section, reading in zip(section['reading_sections'], section['readings']):
+#             assert suzuki.extract_reading(html_section) == reading
+
+
+# def test_build_accent_dict(test_dict: TestDict):
+#     """
+#     - GIVEN html sections
+#     - WHEN the accent dict is constructed
+#     - THEN check all the values are as expected
+#     """
+#     word_sections = [
+#         (section['writing_section'], section['reading_sections'])
+#         for section in test_dict['suzuki']['expected_sections']
+#     ]
+
+#     assert suzuki.build_accent_dict(word_sections) == test_dict['suzuki']['full_accent_dict']
+
+
+# def test_get_accent_dict(monkeypatch, test_dict: TestDict):
+#     """
+#     - GIVEN a list of words
+#     - WHEN the accent dict is generated
+#     - THEN check all the suzuki info is correct and complete
+#     """
+#     word_list = test_dict['input']
+#     html = test_dict['suzuki']['html']
+#     expected_output = test_dict['suzuki']['expected_output']
+
+#     monkeypatch.setattr("requests.post", lambda x, timeout: FakeResponse(html))
+#     assert suzuki.get_accent_dict(word_list) == expected_output
