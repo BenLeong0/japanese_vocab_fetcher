ef44ec43dbb3bd3069583e28ec3d59e7430374eb
benleong0 <benleong@urbanjungle.com>
2021-09-12 15:49:58 +0100

    empty return

--- python_backend/modules/ojad.py
+++ python_backend/modules/ojad.py
@@ -2,4 +2,7 @@ from typing import Dict, List
 
 
 def get_accent_dict(word_list: List[str]) -> Dict[str, List[str]]:
+    if not word_list:
+        return {}
+
     return {key:[] for key in word_list}
