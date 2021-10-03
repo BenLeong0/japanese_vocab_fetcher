ee2bcda69399b97403f20be1d19b535bbee30208
Ben Leong <benleong0@gmail.com>
2021-10-03 16:44:41 +0100

    reorder imports

--- python_backend/testing/test_coordinator.py
+++ python_backend/testing/test_coordinator.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
-from typing import Any, DefaultDict, Dict
 import pytest
+from typing import Any, DefaultDict, Dict
 
 import coordinator
 from custom_types import Kaki
