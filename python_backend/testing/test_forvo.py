b3a16ba59f8e6659cc9ab233290c0291a8bbf771
Ben Leong <benleong0@gmail.com>
2021-10-03 18:29:59 +0100

    fake api key?

--- python_backend/testing/test_forvo.py
+++ python_backend/testing/test_forvo.py
@@ -3,12 +3,20 @@ import re
 from typing import List
 
 from bs4 import BeautifulSoup as Soup
+from dotenv import dotenv_values
 import pytest   # type: ignore
 
 from modules import forvo
 from testing.dict_typing import FullTestDict
 from testing.dicts import TEST_DICTS, TEST_DICT_IDS
 from utils import convert_list_of_str_to_kaki
+
+try:
+    dotenv_values()['FORVO_API_KEY']
+except KeyError:
+    @pytest.fixture(autouse=True)
+    def fake_api_keys(monkeypatch):
+        monkeypatch.setattr("dotenv.dotenv_values", lambda: {"FORVO_API_KEY": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"})
 
 
 # For each test, try with every dict in TEST_DICTS
@@ -38,7 +46,6 @@ def _get_forvo_html_string(url: str, htmls: List[Soup], timeout: int = 20) -> Fa
 
 def test_api_key_import():
     """Check the API_KEY is successfully imported"""
-    assert forvo.API_KEY[:4] == "0300"
     assert len(forvo.API_KEY) == 32
 
 
