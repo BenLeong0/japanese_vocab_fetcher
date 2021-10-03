6cebda5e59a0ed026106eb7646c05d2b9e19a4a2
Ben Leong <benleong0@gmail.com>
2021-10-03 19:24:02 +0100

    api url and tests

--- python_backend/modules/forvo.py
+++ python_backend/modules/forvo.py
@@ -4,6 +4,7 @@ from typing import Dict, List
 from dotenv import dotenv_values
 
 from custom_types import Kaki, URL
+from utils import escape_unicode
 
 
 NAME = "forvo"
@@ -15,4 +16,22 @@ else:
 
 
 def main(word_list: List[Kaki]) -> Dict[Kaki, List[URL]]:
-    return {key:[] for key in word_list}
+    return {word:get_audio_urls(word) for word in word_list}
+
+
+def get_audio_urls(word: Kaki) -> List[URL]:
+    api_url = get_api_url(word)
+    return []
+
+
+def get_api_url(word: Kaki) -> URL:
+    escaped_word = escape_unicode(word)
+    url = \
+        "https://apifree.forvo.com"\
+        "/action/word-pronunciations"\
+        "/format/json"\
+        "/word/%s"\
+        "/id_lang_speak/76"\
+        "/key/%s"\
+        % (escaped_word, API_KEY)
+    return URL(url)
--- python_backend/testing/test_forvo.py
+++ python_backend/testing/test_forvo.py
@@ -3,6 +3,7 @@ import pytest   # type: ignore
 from modules import forvo
 from testing.dict_typing import FullTestDict
 from testing.dicts import TEST_DICTS, TEST_DICT_IDS
+from utils import convert_list_of_str_to_kaki
 
 
 # For each test, try with every dict in TEST_DICTS
@@ -37,3 +38,16 @@ def test_main(monkeypatch, test_dict: FullTestDict):
 
 #     monkeypatch.setattr("requests.post", lambda url, formdata, timeout: FakeResponse(html))
 #     assert forvo.main(word_list) == expected_output
+
+
+def test_get_api_urls(test_dict: FullTestDict):
+    """
+    - GIVEN a list of words
+    - WHEN API urls are generated
+    - THEN check the urls are encoded and correct
+    """
+    word_list = convert_list_of_str_to_kaki(test_dict['input'])
+    expected_urls = [section['url'] for section in test_dict['forvo']['expected_sections']]
+
+    for word, expected_url in zip(word_list, expected_urls):
+        assert forvo.get_api_url(word) == expected_url
