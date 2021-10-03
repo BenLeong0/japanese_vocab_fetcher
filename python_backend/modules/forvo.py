7868965181c6d0f17823b97516373f4052e28aec
Ben Leong <benleong0@gmail.com>
2021-10-03 18:32:54 +0100

    apikey into fn

--- python_backend/modules/forvo.py
+++ python_backend/modules/forvo.py
@@ -6,7 +6,7 @@ from custom_types import Kaki, URL
 
 
 NAME = "forvo"
-API_KEY: str = dotenv_values()['FORVO_API_KEY']
 
 def main(word_list: List[Kaki]) -> Dict[Kaki, List[URL]]:
+    API_KEY: str = dotenv_values()['FORVO_API_KEY']
     return {key:[] for key in word_list}
