a7f36c4c5f3e834e3601f98e5fd780593c4dad15
benleong0 <benleong@urbanjungle.com>
2021-10-07 15:18:24 +0100

    test_create_successful_response

--- python_backend/testing/test_utils.py
+++ python_backend/testing/test_utils.py
@@ -19,3 +19,31 @@ class FakeRequest:
 )
 def test_get_words_from_request(fake_request, expected_result):
     assert utils.get_words_from_request(fake_request) == expected_result
+
+
+@pytest.mark.parametrize(
+    "payload, expected_data",
+    [
+        [[], '[]'],
+        [[{"word": "食べる"}], '[{"word": "食べる"}]'],
+        [[{"word": "食べる"}, {"word": "学生"}], '[{"word": "食べる"}, {"word": "学生"}]'],
+        [
+            [
+                {
+                    "word": "食べる",
+                    "accent": {
+                        "wanikani": ["url1", "url2"]
+                    },
+                }
+            ],
+            r'[{"word": "食べる", "accent": {"wanikani": ["url1", "url2"]}}]'
+        ],
+        ["test_string", '"test_string"'],
+        [{"test": "dictionary"}, '{"test": "dictionary"}'],
+    ]
+)
+def test_create_successful_response(payload, expected_data):
+    response = utils.create_successful_response(payload)
+
+    assert response.data.decode("unicode-escape") == expected_data
+    assert response.status == "200 OK"
