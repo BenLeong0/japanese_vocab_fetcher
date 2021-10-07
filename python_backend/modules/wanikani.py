77a94c48883609abe4e9cc49781b4d30d192ed45
benleong0 <benleong@urbanjungle.com>
2021-10-07 17:21:33 +0100

    get url

--- python_backend/modules/wanikani.py
+++ python_backend/modules/wanikani.py
@@ -9,4 +9,19 @@ NAME = "wanikani"
 API_KEY: str = dotenv_values()['WANIKANI_API_KEY']
 
 def main(word_list: List[Kaki]) -> Dict[Kaki, List[URL]]:
+    if not word_list:
+        return {}
+
+    api_response = get_api_response(word_list)
+
     return {key:[] for key in word_list}
+
+
+def get_api_response(word_list: List[Kaki]) -> Dict:
+    url = get_url(word_list)
+
+
+def get_url(word_list: List[Kaki]) -> URL:
+    slugs = ','.join(word_list)
+    url = f"https://api.wanikani.com/v2/subjects/?slugs={slugs}"
+    return URL(url)
