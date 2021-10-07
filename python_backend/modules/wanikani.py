e0329cfd13831a4b805ec0fa79ce7e568b7a9425
benleong0 <benleong@urbanjungle.com>
2021-10-07 17:03:26 +0100

    read env

--- python_backend/modules/wanikani.py
+++ python_backend/modules/wanikani.py
@@ -1,9 +1,12 @@
 from typing import Dict, List
 
+from dotenv import dotenv_values
+
 from custom_types import Kaki, URL
 
 
 NAME = "wanikani"
+API_KEY: str = dotenv_values()['WANIKANI_API_KEY']
 
 def main(word_list: List[Kaki]) -> Dict[Kaki, List[URL]]:
     return {key:[] for key in word_list}
