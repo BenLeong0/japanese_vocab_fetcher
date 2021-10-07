6d6401199546b8d71bec74b4dfb8bcc9d393e512
benleong0 <benleong@urbanjungle.com>
2021-10-07 15:19:04 +0100

    test_create_failed_response

--- python_backend/testing/test_utils.py
+++ python_backend/testing/test_utils.py
@@ -47,3 +47,31 @@ def test_create_successful_response(payload, expected_data):
 
     assert response.data.decode("unicode-escape") == expected_data
     assert response.status == "200 OK"
+
+
+@pytest.mark.parametrize(
+    "payload, expected_data",
+    [
+        [[], '[]'],
+        [[{"word": "食べる"}], '[{"word": "食べる"}]'],
+        [[{"word": "食べる"}, {"word": "学生"}], '[{"word": "食べる"}, {"word": "学生"}]'],
+        [
+            [
+                {
+                    "word": "食べる",
+                    "accent": {
+                        "wanikani": ["url1", "url2"]
+                    },
+                }
+            ],
+            r'[{"word": "食べる", "accent": {"wanikani": ["url1", "url2"]}}]'
+        ],
+        ["test_string", '"test_string"'],
+        [{"test": "dictionary"}, '{"test": "dictionary"}'],
+    ]
+)
+def test_create_failed_response(payload, expected_data):
+    response = utils.create_failed_response(payload)
+
+    assert response.data.decode("unicode-escape") == expected_data
+    assert response.status == "400 BAD REQUEST"
