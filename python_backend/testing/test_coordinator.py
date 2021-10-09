c70659be368f63d8f907b93f23b609ece244f038
Ben Leong <benleong0@gmail.com>
2021-10-09 19:54:34 +0100

    import order

--- python_backend/testing/test_coordinator.py
+++ python_backend/testing/test_coordinator.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
-import pytest   # type: ignore
 from typing import Any, DefaultDict, Dict
+
+import pytest   # type: ignore
 
 import coordinator
 from custom_types import Kaki
