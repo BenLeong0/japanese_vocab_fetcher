81d397819386c26603a8315460a877f1f84d2966
Ben Leong <benleong0@gmail.com>
2021-10-21 16:03:19 +0100

    create api error type

---
+++ python_backend/custom_types/exception_types.py
@@ -0,0 +1,23 @@
+import json
+
+from custom_types.alternative_string_types import URL
+
+class APIError(Exception):
+    def __init__(
+        self,
+        error_msg: str,
+        status_code: int,
+        url: URL = URL(""),
+    ):
+        super().__init__(error_msg)
+        self.error_msg = error_msg
+        self.status_code = status_code
+        self.url = url
+
+
+    def __str__(self):
+        return json.dumps({
+            "status_code": self.status_code,
+            "error": self.error_msg,
+            "url": self.url,
+        })
