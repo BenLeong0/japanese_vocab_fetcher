bda6c32246a09c8d614352c5f2f7817cb6657f8e
Ben Leong <benleong0@gmail.com>
2021-10-21 17:52:30 +0100

    error repsonse factor

--- python_backend/custom_types/exception_types.py
+++ python_backend/custom_types/exception_types.py
@@ -1,5 +1,5 @@
 import json
-from typing import TypedDict
+from typing import Dict, TypedDict
 
 from custom_types.alternative_string_types import URL
 
@@ -33,3 +33,10 @@ class APIError(Exception):
 
     def __str__(self):
         return json.dumps(self.to_dict())
+
+
+def api_error_response_factory(api_error: APIError) -> Dict:
+    return {
+        "success": False,
+        "error": api_error.to_dict(),
+    }
