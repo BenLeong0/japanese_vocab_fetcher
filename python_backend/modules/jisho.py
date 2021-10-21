94823cac4f93d3e0cec7b8c3a6128797e918236f
Ben Leong <benleong0@gmail.com>
2021-10-21 23:48:09 +0100

    placeholder functions

--- python_backend/modules/jisho.py
+++ python_backend/modules/jisho.py
@@ -1,20 +1,60 @@
+from threading import Thread
 from typing import Dict, List, Union
 
 from custom_types.alternative_string_types import Kaki
-from custom_types.exception_types import FailedResponseItem
-from custom_types.response_types import ResponseItemJisho
+from custom_types.exception_types import APIError, FailedResponseItem, api_error_response_factory
+from custom_types.jisho_api_types import JishoAPIItem, JishoAPIResponse
+from custom_types.response_types import ResponseItemJisho, JishoMainData
 
 
 NAME = "jisho"
 JishoModuleReturnTypes = Union[ResponseItemJisho, FailedResponseItem]
 
 
-def response_factory(success: bool = True) -> ResponseItemJisho:
+def response_factory(data: JishoMainData) -> ResponseItemJisho:
     return {
-        "success": success,
-        "main_data": {
-        },
+        "success": True,
+        "main_data": data,
     }
 
+
+class JishoAPIError(APIError):
+    pass
+
+
 def main(word_list: List[Kaki]) -> Dict[Kaki, JishoModuleReturnTypes]:
-    return {key : response_factory() for key in word_list}
+    results_dict: Dict[Kaki, JishoModuleReturnTypes] = {}
+
+    # def call_script(word: Kaki) -> None:
+    #     results_dict[word] = get_jisho_data(word)
+
+    # threads: List[Thread] = [
+    #     Thread(target=call_script, args=[word])
+    #     for word in word_list
+    # ]
+
+    # for thread in threads:
+    #     thread.start()
+    # for thread in threads:
+    #     thread.join()
+
+    return results_dict
+
+
+def get_jisho_data(word: Kaki) -> JishoModuleReturnTypes:
+    try:
+        response = call_api(word)
+    except JishoAPIError as api_error:
+        print("An error occurred:", api_error.error_msg)
+        return api_error_response_factory(api_error)
+
+    data = extract_jisho_data(response, word)
+    return response_factory(data)
+
+
+def call_api(word: Kaki) -> JishoAPIResponse:
+    ...
+
+
+def extract_jisho_data(response: JishoAPIResponse, word: Kaki) -> JishoMainData:
+    ...
