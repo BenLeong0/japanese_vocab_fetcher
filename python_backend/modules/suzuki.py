8c4d0cecbe4b7bbbd23df4c5b81279e1438762fb
Ben Leong <benleong0@gmail.com>
2021-10-21 15:27:53 +0100

    suzuki

--- python_backend/modules/suzuki.py
+++ python_backend/modules/suzuki.py
@@ -6,18 +6,33 @@ from bs4 import BeautifulSoup as Soup
 import requests
 
 from custom_types.alternative_string_types import HTMLString, Kaki, URL, Yomi
+from custom_types.response_types import ResponseItemSuzuki
 from utils import make_single_line
 
 
 NAME = "suzuki"
 
-def main(word_list: List[Kaki]) -> Dict[Kaki, List[Yomi]]:
+
+def result_factory(accent_list: List[Yomi] = None, success: bool = True) -> ResponseItemSuzuki:
+    if accent_list is None:
+        accent_list = []
+    return {
+        "success": success,
+        "main_data": {
+            "accent": accent_list,
+        },
+    }
+
+
+def main(word_list: List[Kaki]) -> Dict[Kaki, ResponseItemSuzuki]:
     if not word_list:
         return {}
 
     html = get_html(word_list)
     word_sections = get_sections(html)
-    return build_accent_dict(word_sections)
+    accent_dict = build_accent_dict(word_sections)
+
+    return {word:result_factory(accent_list=accent_dict[word]) for word in word_list}
 
 
 # Get HTML
