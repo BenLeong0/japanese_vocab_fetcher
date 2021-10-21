e924ce4bd39284950132aca0e140b24c0b573717
Ben Leong <benleong0@gmail.com>
2021-10-21 23:48:03 +0100

    apitype

---
+++ python_backend/custom_types/jisho_api_types.py
@@ -0,0 +1,41 @@
+from typing import Literal, TypedDict
+
+from custom_types.alternative_string_types import Kaki, URL, Yomi
+
+
+class JishoAPIItemJapanese(TypedDict):
+    word: Kaki
+    reading: Yomi
+
+
+class JishoAPIItemSense(TypedDict):
+    english_definitions: list[str]
+    parts_of_speech: list[str]
+    links: list[URL]
+    tags: list[str]
+    restrictions: list[Yomi]
+    see_also: list[str]
+    antonyms: list[Yomi]
+    source: list[str]
+    info: list[str]
+
+
+class JishoAPIItemAttribution(TypedDict):
+    jmdict: bool
+    jmnedict: bool
+    dbpedia: URL
+
+
+class JishoAPIItem(TypedDict):
+    slug: Kaki
+    is_common: bool
+    tags: list[str]
+    jlpt: list[str]
+    japanese: list[JishoAPIItemJapanese]
+    senses: list[JishoAPIItemSense]
+    attribution: dict
+
+
+class JishoAPIResponse(TypedDict):
+    meta: dict[Literal["status"], int]
+    data: list[JishoAPIItem]
