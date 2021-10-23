ac536f594dddfddca7d3c7d1da92cfac6056a840
Ben Leong <benleong0@gmail.com>
2021-10-23 17:24:09 +0100

    dont print resp in server

--- python_backend/app.py
+++ python_backend/app.py
@@ -14,7 +14,6 @@ CORS(app)
 def homepage():
     word_list = utils.get_words_from_request(request)
     resp = get_info(word_list)
-    print(json.dumps(resp, ensure_ascii=False, indent=4))
     return utils.create_successful_response(resp)
 
 
