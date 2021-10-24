bff3abbf3abbb02562da2add8620f385f156b819
Ben Leong <benleong0@gmail.com>
2021-10-24 23:32:02 +0100

    script

--- .github/workflows/.github-build.sh
+++ .github/workflows/.github-build.sh
@@ -12,11 +12,15 @@ VERSION="$1"
 
 cd python_backend
 
-echo 'run docker build urbanjungle/springer:$BUILD_ENV-$VERSION-$PROVIDER_MODULE'
-docker build --build-arg rmq_host=$RMQ_HOST --build-arg mode=$SERVICE_MODE --rm -t urbanjungle/springer:$BUILD_ENV-$VERSION-$PROVIDER_MODULE .
-echo 'DOCKER BUILD COMPLETE'
-docker push urbanjungle/springer:$BUILD_ENV-$VERSION-$PROVIDER_MODULE
-echo 'DOCKER PUSH COMPLETE'
-echo 'DOCKER BUILDING AND PUSHING FINISHED'
+ls
+
+echo '$VERSION'
+
+# echo 'run docker build urbanjungle/springer:$BUILD_ENV-$VERSION-$PROVIDER_MODULE'
+# docker build --build-arg rmq_host=$RMQ_HOST --build-arg mode=$SERVICE_MODE --rm -t urbanjungle/springer:$BUILD_ENV-$VERSION-$PROVIDER_MODULE .
+# echo 'DOCKER BUILD COMPLETE'
+# docker push urbanjungle/springer:$BUILD_ENV-$VERSION-$PROVIDER_MODULE
+# echo 'DOCKER PUSH COMPLETE'
+# echo 'DOCKER BUILDING AND PUSHING FINISHED'
 
 echo "FINISHED"
