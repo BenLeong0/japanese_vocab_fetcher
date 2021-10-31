1dd622bbae7234f23b4c3d11a4528d88eef71c2f
Ben Leong <benleong0@gmail.com>
2021-10-31 17:08:11 +0000

    env stuff

--- .github-build.sh
+++ .github-build.sh
@@ -2,18 +2,20 @@
 set -e
 
 #check number of args
-if ! [[ $# -eq 1 ]];
+if ! [[ $# -eq 3 ]];
 then
-  echo "VERSION ARGUMENT REQUIRED"
+  echo "ARGUMENTS REQUIRED: VERSION, FORVO API KEY AND WANIKANI API KEY"
   exit 1
 fi
 
 VERSION="$1"
+FORVO_API_KEY="$2"
+WANIKANI_API_KEY="$3"
 
 cd python_backend
 
 echo 'run docker build urbanjungle/springer:$BUILD_ENV-$VERSION-$PROVIDER_MODULE'
-docker build -t benleong0/japanese_vocab_fetcher:$VERSION .
+docker build -t benleong0/japanese_vocab_fetcher:$VERSION . forvo_api_key=$FORVO_API_KEY wanikani_api_key=$WANIKANI_API_KEY
 echo 'DOCKER BUILD COMPLETE'
 docker push benleong0/japanese_vocab_fetcher:$VERSION
 echo 'DOCKER PUSH COMPLETE'
