3701b1a1ca74624adc98632ee768502c0a483fa9
Ben Leong <benleong0@gmail.com>
2021-11-07 15:36:05 +0000

    extract query params to utils service and type file

---
+++ react-interface/src/components/core/HttpService.test.ts
@@ -0,0 +1,17 @@
+import each from "jest-each"
+
+import QueryParams from "../../types/QueryParams";
+import UtilsService from "./UtilsService";
+
+const utilsService = new UtilsService();
+
+describe("addQueryParamsToUrl works", () => {
+    const url = "www.text.com/example"
+    each([
+        [{word: "hi"}, "www.text.com/example?word=hi"],
+        [{word: "hi", colour: "red"}, "www.text.com/example?word=hi&colour=red"],
+        [{word: "hi", colour: "red", hi: "5"}, "www.text.com/example?word=hi&colour=red&hi=5"],
+    ]).it("with the params '%s'", (params: QueryParams, expected: string) => {
+        expect(utilsService.addQueryParamsToUrl(url, params)).toBe(expected)
+    })
+});
