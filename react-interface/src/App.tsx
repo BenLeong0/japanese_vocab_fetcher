06c5563de2892ed18c157b9a167938af3d002d87
benleong0 <benleong@urbanjungle.com>
2021-11-08 21:28:18 +0000

    wordlist + indent + results page

--- react-interface/src/App.tsx
+++ react-interface/src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputPage from './components/InputPage/InputPage';
+import ResultsPage from './components/ResultsPage/ResultsPage';
+import FullResponseItem from './types/FullResponseItem';
+
 
 function App() {
-  return (
-    <div className="App">
-      <div className="width-container">
-        <p className="app-header">Japanese Vocab Fetcher</p>
-        <InputPage />
-      </div>
-    </div>
-  );
+    const [wordList, setWordList] = useState<FullResponseItem[]>([])
+
+    return (
+        <div className="App">
+            <div className="width-container">
+                <p className="app-header">Japanese Vocab Fetcher</p>
+                <InputPage setWordList={setWordList} />
+                <ResultsPage wordList={wordList} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
