117014cf3bed16ac7f26219deac26d85c75468e5
benleong0 <benleong@myurbanjungle.com>
2021-12-26 21:15:16 +0000

    inputbox to own component

---
+++ react-interface/src/components/InputPage/InputBox/InputBox.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
+
+import FullResponseItem from '../../../types/FullResponseItem';
+import QueryParams from '../../../types/QueryParams';
+
+import HttpService from '../../core/HttpService';
+import UtilsService from '../../core/UtilsService';
+
+import './InputBox.css';
+
+interface InputBoxProps {
+    setWordList: (s: FullResponseItem[]) => void;
+    setErrorOccurred: (x: boolean) => void;
+}
+
+const InputBox: React.FC<InputBoxProps> = ({ setWordList, setErrorOccurred }) => {
+    const httpService = new HttpService();
+    const utilsService = new UtilsService();
+
+    const [text, setText] = useState<string>('');
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const sendWords = async (event: React.FormEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+
+        setIsLoading(true);
+        setErrorOccurred(false);
+
+        const words: string[] = utilsService.extractWordsFromInput(text);
+        const queryParams: QueryParams = {
+            words: JSON.stringify(words),
+        };
+
+        try {
+            const resp = await httpService.makeGetRequest("/words", queryParams);
+            setWordList(resp);
+            console.log(resp);
+        }
+        catch (error) {
+            setErrorOccurred(true);
+            console.log(error);
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+
+    const textArea = (
+        <TextareaAutosize
+            name="main-input"
+            className="main-input"
+            value={text}
+            onChange={(e: any) => setText(e.target.value)}
+        />
+    )
+    const wordsDisplay = (
+        <div className="words-display vertical-separation-small">
+            {utilsService.extractWordsFromInput(text).map((word) =>
+                <div className="word-display">{word}</div>
+            )}
+        </div>
+    )
+    const submitButton = (
+        <button
+            className="button-primary vertical-separation-medium"
+            type="submit"
+            onClick={sendWords}
+        >
+            Submit
+        </button>
+    )
+
+    const loadingSpinner = <div className="loader vertical-separation-large"></div>;
+
+    return (
+        isLoading ? loadingSpinner : <>{textArea}{wordsDisplay}{submitButton}</>
+    );
+}
+
+export default InputBox;
--- react-interface/src/components/InputPage/InputPage.tsx
+++ react-interface/src/components/InputPage/InputPage.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
-import TextareaAutosize from 'react-textarea-autosize';
-import FullResponseItem from '../../types/FullResponseItem';
+import InputBox from './InputBox/InputBox';
 
-import QueryParams from '../../types/QueryParams';
-import HttpService from '../core/HttpService';
+import FullResponseItem from '../../types/FullResponseItem';
 
 import ErrorIcon from "../../assets/icons/icon_error.svg";
 import './InputPage.css';
@@ -14,77 +12,20 @@ export interface InputPageProps {
 }
 
 const InputPage: React.FC<InputPageProps> = ({ setWordList }) => {
-    const httpService = new HttpService();
-
-    const [text, setText] = useState<string>('');
-    const [isLoading, setIsLoading] = useState<boolean>(false);
     const [errorOccurred, setErrorOccurred] = useState<boolean>(false);
 
-    const sendWords = async (event: React.FormEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-        setIsLoading(true);
-        setErrorOccurred(false);
-
-        const words: string[] = getWords(text);
-        const wordsString: string = JSON.stringify(words);
-
-        const queryParams: QueryParams = {
-            words: wordsString,
-        };
-
-        try {
-            const resp = await httpService.makeGetRequest("/worsdfds", queryParams);
-            console.log(resp);
-            setWordList(resp);
-        }
-        catch (error) {
-            console.log(error);
-            setErrorOccurred(true);
-        }
-        setIsLoading(false);
-    }
-
-    const errorMessageDisplay = () => {
-        return (
-            errorOccurred ?
-            <div className="error-message vertical-separation-large">
-                <img src={ErrorIcon} alt="error icon" />
-                An error occurred. Please try again
-                <img src={ErrorIcon} alt="error icon" />
-            </div> :
-            <></>
-        );
-    }
-    const getInputBox = () => {
-        if (isLoading) {
-            return <div className="loader vertical-separation-large"></div>;
-        }
-        return (
-            <>
-                <TextareaAutosize
-                    name="main-input"
-                    className="main-input"
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-                />
-                <div className="words-display vertical-separation-small">
-                    {getWords(text).map((word) =>
-                        <div className="word-display">{word}</div>
-                    )}
-                </div>
-                <button
-                    className="button-primary vertical-separation-medium"
-                    type="submit"
-                    onClick={sendWords}
-                >Submit</button>
-            </>
-        );
-    }
+    const errorMessage = (
+        <div className="error-message vertical-separation-large">
+            <img src={ErrorIcon} alt="error icon" />
+            An error occurred. Please try again
+            <img src={ErrorIcon} alt="error icon" />
+        </div>
+    );
 
     return (
         <div className="input-page">
-            { getInputBox() }
-            { errorMessageDisplay() }
+            <InputBox setWordList={setWordList} setErrorOccurred={setErrorOccurred}/>
+            { errorOccurred ? errorMessage : <></> }
         </div>
     );
 }
