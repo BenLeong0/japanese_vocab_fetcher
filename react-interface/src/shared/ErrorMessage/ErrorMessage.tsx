f7de970b2d46e577b225fddf0933d48ceefdfc0b
benleong0 <benleong@myurbanjungle.com>
2021-12-26 22:48:04 +0000

    error message to shared + tests

---
+++ react-interface/src/shared/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import ErrorIcon from "../../assets/icons/icon_error.svg";
+
+
+interface ErrorMessageProps {
+    children: string;
+}
+
+const ErrorMessage: React.FunctionComponent<ErrorMessageProps> = ({ children }) => {
+    return (
+        <div className="error-message vertical-separation-large">
+            <img src={ErrorIcon} alt="error icon" />
+            { children }
+            <img src={ErrorIcon} alt="error icon" />
+        </div>
+    );
+}
+
+export default ErrorMessage;
