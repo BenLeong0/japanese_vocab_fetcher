2ec2c68ce0c48ab24f3d0a34a47e3e71df797362
benleong0 <benleong@myurbanjungle.com>
2021-12-26 20:00:27 +0000

    error handling

--- react-interface/src/components/InputPage/InputPage.tsx
+++ react-interface/src/components/InputPage/InputPage.tsx
@@ -5,6 +5,7 @@ import FullResponseItem from '../../types/FullResponseItem';
 import QueryParams from '../../types/QueryParams';
 import HttpService from '../core/HttpService';
 
+import ErrorIcon from "../../assets/icons/icon_error.svg";
 import './InputPage.css';
 
 
@@ -14,12 +15,15 @@ export interface InputPageProps {
 
 const InputPage: React.FC<InputPageProps> = ({ setWordList }) => {
     const httpService = new HttpService();
+
     const [text, setText] = useState<string>('');
-    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [errorOccurred, setErrorOccurred] = useState<boolean>(false);
 
     const sendWords = async (event: React.FormEvent<HTMLButtonElement>) => {
         event.preventDefault();
         setIsLoading(true);
+        setErrorOccurred(false);
 
         const words: string[] = getWords(text);
         const wordsString: string = JSON.stringify(words);
@@ -27,30 +31,60 @@ const InputPage: React.FC<InputPageProps> = ({ setWordList }) => {
         const queryParams: QueryParams = {
             words: wordsString,
         };
-        const resp = await httpService.makeGetRequest("/words", queryParams);
+
+        try {
+            const resp = await httpService.makeGetRequest("/worsdfds", queryParams);
+            console.log(resp);
+            setWordList(resp);
+        }
+        catch (error) {
+            console.log(error);
+            setErrorOccurred(true);
+        }
         setIsLoading(false);
-        console.log(resp);
-        setWordList(resp);
     }
 
-    return (
-        <div className="input-page">
-            { !isLoading ? (
-                <>
+    const errorMessageDisplay = () => {
+        return (
+            errorOccurred ?
+            <div className="error-message vertical-separation-large">
+                <img src={ErrorIcon} alt="error icon" />
+                An error occurred. Please try again
+                <img src={ErrorIcon} alt="error icon" />
+            </div> :
+            <></>
+        );
+    }
+    const getInputBox = () => {
+        if (isLoading) {
+            return <div className="loader vertical-separation-large"></div>;
+        }
+        return (
+            <>
                 <TextareaAutosize
                     name="main-input"
                     className="main-input"
                     value={text}
                     onChange={(e) => setText(e.target.value)}
                 />
-                <div className="words-display">
+                <div className="words-display vertical-separation-small">
                     {getWords(text).map((word) =>
                         <div className="word-display">{word}</div>
                     )}
                 </div>
-                <button className="button-primary" type="submit" onClick={sendWords}>Submit</button></>
-            ) : <div></div>
-            }
+                <button
+                    className="button-primary vertical-separation-medium"
+                    type="submit"
+                    onClick={sendWords}
+                >Submit</button>
+            </>
+        );
+    }
+
+    return (
+        <div className="input-page">
+            { getInputBox() }
+            { errorMessageDisplay() }
         </div>
     );
 }
