56f11b06e54d03e43219de8bfba762a159b20023
benleong0 <benleong@myurbanjungle.com>
2021-12-26 22:10:26 +0000

    spacing and scolons

--- react-interface/src/components/core/UtilsService.test.ts
+++ react-interface/src/components/core/UtilsService.test.ts
@@ -1,19 +1,20 @@
-import each from "jest-each"
+import each from "jest-each";
 
 import QueryParams from "../../types/QueryParams";
+
 import UtilsService from "./UtilsService";
 
 
 const utilsService = new UtilsService();
 
 describe("addQueryParamsToUrl works", () => {
-    const url = "www.text.com/example"
+    const url = "www.text.com/example";
     each([
         [{word: "hi"}, "www.text.com/example?word=hi"],
         [{word: "hi", colour: "red"}, "www.text.com/example?word=hi&colour=red"],
         [{word: "hi", colour: "red", hi: "5"}, "www.text.com/example?word=hi&colour=red&hi=5"],
     ]).it("with the params '%s'", (params: QueryParams, expected: string) => {
-        expect(utilsService.addQueryParamsToUrl(url, params)).toBe(expected)
+        expect(utilsService.addQueryParamsToUrl(url, params)).toBe(expected);
     });
 });
 
@@ -28,6 +29,6 @@ describe('extract words from input', () => {
         ["test \n word", ["test", "word"]],
         ["a     test \n word", ["a", "test", "word"]],
     ]).it("with the input '%s'", (s: string, expected: string[]) => {
-        expect(utilsService.extractWordsFromInput(s)).toEqual(expected)
+        expect(utilsService.extractWordsFromInput(s)).toEqual(expected);
     });
 });
