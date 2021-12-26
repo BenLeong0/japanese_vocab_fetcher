217b4e88c6f8ede4025a0f5814fccb0e4bb8702d
benleong0 <benleong@myurbanjungle.com>
2021-12-26 22:32:21 +0000

    sclonon

--- react-interface/src/components/core/HttpService.test.ts
+++ react-interface/src/components/core/HttpService.test.ts
@@ -1,4 +1,4 @@
-import each from "jest-each"
+import each from "jest-each";
 
 import QueryParams from "../../types/QueryParams";
 import UtilsService from "./UtilsService";
@@ -12,6 +12,6 @@ describe("addQueryParamsToUrl works", () => {
         [{word: "hi", colour: "red"}, "www.text.com/example?word=hi&colour=red"],
         [{word: "hi", colour: "red", hi: "5"}, "www.text.com/example?word=hi&colour=red&hi=5"],
     ]).it("with the params '%s'", (params: QueryParams, expected: string) => {
-        expect(utilsService.addQueryParamsToUrl(url, params)).toBe(expected)
+        expect(utilsService.addQueryParamsToUrl(url, params)).toBe(expected);
     })
 });
