5ff50c7491d36968cece9ddd4cb033dc27118e66
benleong0 <benleong@myurbanjungle.com>
2021-12-27 12:18:07 +0000

    columns

--- react-interface/src/components/ResultsList/ResultBlock.tsx
+++ react-interface/src/components/ResultsList/ResultBlock.tsx
@@ -14,8 +14,17 @@ const Result: React.FunctionComponent<ResultProps> = ({ data }) => {
     // const wanikaniData = data.wanikani;
 
     return (
-        <div className="result-block">
+        <div className="result-block flex-col">
             <ResultTitle>{ data.word }</ResultTitle>
+            <div className="flex-row">
+                <div className="result-left-col flex-col">
+                    left
+                </div>
+                <div className="result-col-separator">&nbsp;</div>
+                <div className="result-right-col flex-col">
+                    right
+                </div>
+            </div>
         </div>
     );
 }
