7a353150ef4599b0bbd54bbee2493d663af25151
benleong0 <benleong@myurbanjungle.com>
2021-12-27 17:17:47 +0000

    import and import order

--- react-interface/src/components/ResultsList/ResultTags/ResultTags.tsx
+++ react-interface/src/components/ResultsList/ResultTags/ResultTags.tsx
@@ -7,13 +7,36 @@ interface ResultTagProps {
     data: FullResponseItem;
 }
 
-const ResultTag: React.FC<ResultTagProps> = ({ data }) => {
+const ResultTags: React.FC<ResultTagProps> = ({ data }) => {
+    const tags = extractTags(data);
 
     return ( <>
-        { data.jisho.main_data.results.map(result => result.tags.map(tag =>
+        { tags.map(tag =>
             <div className="result-tag">{ tag }</div>
-        ))}
+        )}
     </> );
 }
 
-export default ResultTag;
+
+const extractTags = (data: FullResponseItem): string[] => {
+    let tags: string[] = [];
+    for (let result of data.jisho.main_data.results) {
+        for (let jlptLevel of result.jlpt) {
+            tags.push(jlptLevel.toUpperCase().replace("-", " "));
+        }
+        for (let tag of result.tags) {
+            if (tag.slice(0, 8) === "wanikani") {
+                tags.push(`Wanikani level ${tag.slice(8)}`);
+            }
+            else if (tag === "common-word") {
+                tags.push("Common word");
+            }
+            else {
+                tags.push(tag);
+            }
+        }
+    }
+    return tags;
+}
+
+export default ResultTags;
