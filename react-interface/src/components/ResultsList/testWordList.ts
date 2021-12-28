bd2c96d20a5d6c1e14d9ef57cfc0c403ed114c75
Ben Leong <benleong0@gmail.com>
2021-12-28 16:23:36 +0000

    test word list

---
+++ react-interface/src/components/ResultsList/testWordList.ts
@@ -0,0 +1,564 @@
+import FullResponseItem from "../../types/FullResponseItem";
+
+const testWordList: FullResponseItem[] = [
+  {
+    word: "\u5b66\u751f",
+    forvo: {
+      success: true,
+      error: null,
+      main_data: {
+        audio: [
+          {
+            url: "https://apifree.forvo.com/audio/24213n2m1o1f393m3o3i1p291m372g2n233e243e3d2f233m331l2f3i1n283i1n2b2d2i2i3c2i1p1p1l3j1m2i3a373d333h31311l351h2l2m3a222o3d3d3b2g3f3m371k2a253g371b323c2q3c2n34343n1i3g2q3h3l211t1t_2o1h3o3o3a322m2p2a393f342l21391f261m2p2j3l3n1t1t",
+            username: "akiko",
+          },
+          {
+            url: "https://apifree.forvo.com/audio/1m3j213o2d3l2q1l353c3h27322d3239372821352j2o2b3e211p283b3d2o263h2e2q2c3e3p3p1f3k2f322h1m2n3m3m2p1p1o2e2h3k272h2l3k1k3p3j3i1b263b381k33251j2m2d2f1g1n2e3b341n3c3c3o39321o252h1t1t_261k2l243j39313d2h2a363m1l3e24263p2j231o23371t1t",
+            username: "Emmacaron",
+          },
+          {
+            url: "https://apifree.forvo.com/audio/2f292d3p1f2j22243c231m363p2j1g3i2c271i341l1i3m1g1m2427331p25321n2m3122243d2p313o232m3d232j2l3o3c29223q2m2n292j2p352j342c2p292e3d242l283f322d3n2b2b2k1g34361j3e1p2n1b3a1n3p371t1t_2b34353l1k1n1o3g1j3n1g242f2d3o3h2i1b2k1k372h1t1t",
+            username: "yasuo",
+          },
+        ],
+      },
+    },
+    jisho: {
+      success: true,
+      error: null,
+      main_data: {
+        results: [
+          {
+            slug: "\u5b66\u751f",
+            is_common: true,
+            tags: ["wanikani5"],
+            jlpt: ["jlpt-n5"],
+            japanese: [
+              { word: "\u5b66\u751f", reading: "\u304c\u304f\u305b\u3044" },
+            ],
+            senses: [
+              {
+                english_definitions: ["student (esp. a university student)"],
+                parts_of_speech: ["Noun"],
+                links: [],
+                tags: [],
+                restrictions: [],
+                see_also: [],
+                antonyms: [],
+                source: [],
+                info: [],
+              },
+            ],
+            attribution: { jmdict: true, jmnedict: false, dbpedia: false },
+          },
+          {
+            slug: "\u5b66\u751f-1",
+            is_common: false,
+            tags: [],
+            jlpt: [],
+            japanese: [
+              {
+                word: "\u5b66\u751f",
+                reading: "\u304c\u304f\u3057\u3087\u3046",
+              },
+              { word: "\u5b66\u751f", reading: "\u304c\u304f\u305d\u3046" },
+            ],
+            senses: [
+              {
+                english_definitions: [
+                  "Heian-period student of government administration",
+                ],
+                parts_of_speech: ["Noun"],
+                links: [],
+                tags: ["Archaism"],
+                restrictions: [],
+                see_also: [],
+                antonyms: [],
+                source: [],
+                info: [],
+              },
+              {
+                english_definitions: [
+                  "Buddhist scholar",
+                  "researcher at a Buddhist temple",
+                  "person studying Buddhism",
+                ],
+                parts_of_speech: ["Noun"],
+                links: [],
+                tags: ["Archaism"],
+                restrictions: [],
+                see_also: [],
+                antonyms: [],
+                source: [],
+                info: [],
+              },
+              {
+                english_definitions: ["learning", "scholarship"],
+                parts_of_speech: ["Noun"],
+                links: [],
+                tags: ["Archaism"],
+                restrictions: [],
+                see_also: [],
+                antonyms: [],
+                source: [],
+                info: [],
+              },
+            ],
+            attribution: { jmdict: true, jmnedict: false, dbpedia: false },
+          },
+        ],
+        extra: [
+          {
+            slug: "\u5b66\u751f\u6642\u4ee3",
+            japanese: [
+              {
+                word: "\u5b66\u751f\u6642\u4ee3",
+                reading: "\u304c\u304f\u305b\u3044\u3058\u3060\u3044",
+              },
+            ],
+          },
+          {
+            slug: "\u5b66\u751f\u904b\u52d5",
+            japanese: [
+              {
+                word: "\u5b66\u751f\u904b\u52d5",
+                reading: "\u304c\u304f\u305b\u3044\u3046\u3093\u3069\u3046",
+              },
+            ],
+          },
+          {
+            slug: "\u5b66\u751f\u751f\u6d3b",
+            japanese: [
+              {
+                word: "\u5b66\u751f\u751f\u6d3b",
+                reading: "\u304c\u304f\u305b\u3044\u305b\u3044\u304b\u3064",
+              },
+            ],
+          },
+          {
+            slug: "\u5b66\u751f\u4f1a",
+            japanese: [
+              {
+                word: "\u5b66\u751f\u4f1a",
+                reading: "\u304c\u304f\u305b\u3044\u304b\u3044",
+              },
+            ],
+          },
+          {
+            slug: "\u5b66\u751f\u670d",
+            japanese: [
+              {
+                word: "\u5b66\u751f\u670d",
+                reading: "\u304c\u304f\u305b\u3044\u3075\u304f",
+              },
+            ],
+          },
+          {
+            slug: "\u5b66\u751f\u8a3c",
+            japanese: [
+              {
+                word: "\u5b66\u751f\u8a3c",
+                reading: "\u304c\u304f\u305b\u3044\u3057\u3087\u3046",
+              },
+            ],
+          },
+          {
+            slug: "\u5b66\u751f\u4f1a\u9928",
+            japanese: [
+              {
+                word: "\u5b66\u751f\u4f1a\u9928",
+                reading: "\u304c\u304f\u305b\u3044\u304b\u3044\u304b\u3093",
+              },
+            ],
+          },
+          {
+            slug: "\u5b66\u751f\u5272\u5f15",
+            japanese: [
+              {
+                word: "\u5b66\u751f\u5272\u5f15",
+                reading: "\u304c\u304f\u305b\u3044\u308f\u308a\u3073\u304d",
+              },
+              {
+                word: "\u5b66\u751f\u5272\u5f15\u304d",
+                reading: "\u304c\u304f\u305b\u3044\u308f\u308a\u3073\u304d",
+              },
+              {
+                word: "\u5b66\u751f\u5272\u308a\u5f15\u304d",
+                reading: "\u304c\u304f\u305b\u3044\u308f\u308a\u3073\u304d",
+              },
+            ],
+          },
+        ],
+      },
+    },
+    ojad: {
+      success: true,
+      error: null,
+      main_data: { accent: ["\u304c\u304f\u305b\u3044"] },
+    },
+    suzuki: {
+      success: true,
+      error: null,
+      main_data: { accent: ["\u304c\u304f\u305b\u3044"] },
+    },
+    wadoku: {
+      success: true,
+      error: null,
+      main_data: { accent: ["\u304c\u304f\u305b\u3044"] },
+    },
+    wanikani: {
+      success: true,
+      error: null,
+      main_data: {
+        audio: [
+          {
+            url: "https://files.wanikani.com/g2cqz8n0yfcy4n0d45sheojq064j",
+            metadata: {
+              gender: "male",
+              source_id: 9511,
+              pronunciation: "\u304c\u304f\u305b\u3044",
+              voice_actor_id: 2,
+              voice_actor_name: "Kenichi",
+              voice_description: "Tokyo accent",
+            },
+            content_type: "audio/mpeg",
+          },
+          {
+            url: "https://files.wanikani.com/l6jqstgdee1l55fnt9906go7fmv1",
+            metadata: {
+              gender: "female",
+              source_id: 26386,
+              pronunciation: "\u304c\u304f\u305b\u3044",
+              voice_actor_id: 1,
+              voice_actor_name: "Kyoko",
+              voice_description: "Tokyo accent",
+            },
+            content_type: "audio/mpeg",
+          },
+        ],
+        sentences: [
+          {
+            en: "Which parts will students study at home today?",
+            ja: "\u5b66\u751f\u306f\u4eca\u65e5\u3001\u3046\u3061\u3067\u3069\u3053\u3092\u3079\u3093\u304d\u3087\u3046\u3057\u307e\u3059\u304b\u3002",
+          },
+          {
+            en: "Do you have student discount?",
+            ja: "\u5b66\u751f\u308f\u308a\u5f15\u3063\u3066\u3042\u308a\u307e\u3059\u304b\uff1f",
+          },
+          {
+            en: '"We are all students of life," my dad said while farting.',
+            ja: "\u300c\u6211\u3005\u306f\u307f\u306a\u4eba\u751f\u306e\u5b66\u751f\u3060\u3002\u300d\u3068\u8a00\u3044\u306a\u304c\u3089\u3001\u7236\u306f\u304a\u306a\u3089\u3092\u3057\u305f\u3002",
+          },
+        ],
+      },
+    },
+  },
+  {
+    word: "\u9759\u304b",
+    forvo: {
+      success: true,
+      error: null,
+      main_data: {
+        audio: [
+          {
+            url: "https://apifree.forvo.com/audio/311m1b22283d1k1b3a2h2j2b2j3h2k2q2d32382m2n2l1k382m281p2o1m3i2q1k1i1f2j22343g1g263p3a1f1n2q2q2p2i2a2h3429282i3e1p232n283g1f3q2i3h3l28381k2a253n2o1m1m1l32332k2d353g3c3i3a2p371t1t_3a2d291n352p1l2k2d2c283j2i3d3o1i2f1i2a2g26211t1t",
+            username: "nohunohu",
+          },
+          {
+            url: "https://apifree.forvo.com/audio/2o2j3c1i1k2l313j333i1p3h293d1m373k1i3p312k3h1o2j331i1n2p1m1j1f3d2d3b2423311g2121253h3i2q2b1f2d3o1f1b391p2m251g2e2j352m2n2j32281h3o3p3b1j2n243n2b2c3j3h242b311j3n353e3l3g312h1t1t_2g233j2o2f2a1l2k1b3p2o253h2g2d2j3l3h1i3b2i211t1t",
+            username: "JunkoHanabi",
+          },
+          {
+            url: "https://apifree.forvo.com/audio/1l3227372e36392i2m1h1g2i1n263l3h1n3i3b2e3h221n1o3h211b331n1i3j321g2m2q262q1n21362i2d3c2l1g3d2l232b382g2b291g1b3a1b2e3c3p2f3j24282h2q2224352d3m3o2a371k2i3a1h3h1m372p3j3a2p3n1t1t_2n3k3n232d1l1f2m271i2p1i3o3d3b2i213j21341n371t1t",
+            username: "strawberrybrown",
+          },
+          {
+            url: "https://apifree.forvo.com/audio/23362k2c22381k2c2l1o31233n3533212q3e3o1f3c273m3g2h2g1p382e3831352f323p3h1n2p392j2q392c34292i2o1f23313h353o3b39392g3f2h2c2l1l2h1l1l2m232d1h2h2m3o3j3d2b2f2h212k1k26392p2q2b211t1t_1p3e25262f382p3f2f2f2b282p3j2p382b3n231i272h1t1t",
+            username: "straycat88",
+          },
+          {
+            url: "https://apifree.forvo.com/audio/1b34353c1o362f2p333b261f3f34253b2h1h2b1k353k1f2i2q3d1k222d3a2n3n2c2m2p2a2l36312k343i1h2m1j3c3p3n3l3j2l3f1j2i1m28332a2a3e1g321p3q2q2e3i2k3q2m3o2f1b392a3j2l2o381l273j2q282e3n1t1t_353b361g2k3b1b242j2i2i3k292e3l2a3c1j3a2234371t1t",
+            username: "skent",
+          },
+        ],
+      },
+    },
+    jisho: {
+      success: true,
+      error: null,
+      main_data: {
+        results: [
+          {
+            slug: "\u9759\u304b",
+            is_common: true,
+            tags: ["wanikani18"],
+            jlpt: ["jlpt-n5"],
+            japanese: [
+              { word: "\u9759\u304b", reading: "\u3057\u305a\u304b" },
+              { word: "\u9591\u304b", reading: "\u3057\u305a\u304b" },
+            ],
+            senses: [
+              {
+                english_definitions: ["quiet", "silent"],
+                parts_of_speech: ["Na-adjective (keiyodoshi)"],
+                links: [],
+                tags: [],
+                restrictions: [],
+                see_also: [],
+                antonyms: [],
+                source: [],
+                info: [],
+              },
+              {
+                english_definitions: ["slow", "unhurried"],
+                parts_of_speech: ["Na-adjective (keiyodoshi)"],
+                links: [],
+                tags: [],
+                restrictions: [],
+                see_also: [],
+                antonyms: [],
+                source: [],
+                info: [],
+              },
+              {
+                english_definitions: ["calm", "peaceful"],
+                parts_of_speech: ["Na-adjective (keiyodoshi)"],
+                links: [],
+                tags: [],
+                restrictions: [],
+                see_also: [],
+                antonyms: [],
+                source: [],
+                info: [],
+              },
+            ],
+            attribution: { jmdict: true, jmnedict: false, dbpedia: false },
+          },
+        ],
+        extra: [
+          {
+            slug: "\u9759\u304b\u306b",
+            japanese: [
+              {
+                word: "\u9759\u304b\u306b",
+                reading: "\u3057\u305a\u304b\u306b",
+              },
+            ],
+          },
+          {
+            slug: "\u9759\u304b\u306e\u6d77",
+            japanese: [
+              {
+                word: "\u9759\u304b\u306e\u6d77",
+                reading: "\u3057\u305a\u304b\u306e\u3046\u307f",
+              },
+            ],
+          },
+          {
+            slug: "\u9759\u304b\u306b\u6d41\u308c\u308b\u5ddd\u306f\u6df1\u3044",
+            japanese: [
+              {
+                word: "\u9759\u304b\u306b\u6d41\u308c\u308b\u5ddd\u306f\u6df1\u3044",
+                reading:
+                  "\u3057\u305a\u304b\u306b\u306a\u304c\u308c\u308b\u304b\u308f\u306f\u3075\u304b\u3044",
+              },
+            ],
+          },
+          {
+            slug: "51869194d5dda7b2c601eed0",
+            japanese: [{ word: "\u9759\u304b\u306a\u308b\u30c9\u30f3" }],
+          },
+          {
+            slug: "518698ead5dda7b2c60558f8",
+            japanese: [
+              {
+                word: "\u9759\u304b\u306a\u6d77\u3068\u697d\u3057\u3044\u822a\u6d77 (\u30d9\u30fc\u30c8\u30fc\u30f4\u30a7\u30f3)",
+              },
+            ],
+          },
+          {
+            slug: "51869907d5dda7b2c60566de",
+            japanese: [
+              {
+                word: "\u9759\u304b\u306a\u6d77\u3068\u697d\u3057\u3044\u822a\u6d77",
+              },
+            ],
+          },
+          {
+            slug: "51869460d5dda7b2c6033553",
+            japanese: [{ word: "\u9759\u304b\u306a\u308b\u6c7a\u95d8" }],
+          },
+          {
+            slug: "51869908d5dda7b2c60566e2",
+            japanese: [
+              {
+                word: "\u9759\u304b\u306a\u6d77\u3068\u697d\u3057\u3044\u822a\u6d77 (\u30e1\u30f3\u30c7\u30eb\u30b9\u30be\u30fc\u30f3)",
+              },
+            ],
+          },
+          {
+            slug: "51869d05d5dda7b2c6074a12",
+            japanese: [
+              { word: "\u9759\u304b\u306a\u30a2\u30e1\u30ea\u30ab\u4eba" },
+            ],
+          },
+        ],
+      },
+    },
+    ojad: {
+      success: true,
+      error: null,
+      main_data: { accent: ["\u3057' \u305a\u304b"] },
+    },
+    suzuki: {
+      success: true,
+      error: null,
+      main_data: { accent: ["\u3057' \u305a\u304b"] },
+    },
+    wadoku: {
+      success: true,
+      error: null,
+      main_data: { accent: ["\u3057' \u305a\u304b"] },
+    },
+    wanikani: {
+      success: true,
+      error: null,
+      main_data: {
+        audio: [
+          {
+            url: "https://files.wanikani.com/awv61bmcmf9w4m7p057jqubhu3f1",
+            metadata: {
+              gender: "male",
+              source_id: 4367,
+              pronunciation: "\u3057\u305a\u304b",
+              voice_actor_id: 2,
+              voice_actor_name: "Kenichi",
+              voice_description: "Tokyo accent",
+            },
+            content_type: "audio/mpeg",
+          },
+          {
+            url: "https://files.wanikani.com/pkutc4gv3nxpmuo1dba6iv6p5luh",
+            metadata: {
+              gender: "female",
+              source_id: 22694,
+              pronunciation: "\u3057\u305a\u304b",
+              voice_actor_id: 1,
+              voice_actor_name: "Kyoko",
+              voice_description: "Tokyo accent",
+            },
+            content_type: "audio/mpeg",
+          },
+        ],
+        sentences: [
+          {
+            en: "We'd prefer a house on a quiet street.",
+            ja: "\u9759\u304b\u306a\u901a\u308a\u306b\u3042\u308b\u5bb6\u306e\u65b9\u304c\u3044\u3044\u3067\u3059\u3002",
+          },
+          {
+            en: "I know you have diarrhea, but could you keep it down a bit more, please?",
+            ja: "\u4e0b\u308a\u306a\u306e\u306f\u5206\u304b\u308a\u307e\u3059\u304c\u3001\u3082\u3046\u5c11\u3057\u9759\u304b\u306b\u3057\u3066\u3044\u305f\u3060\u3051\u307e\u3059\u304b\uff1f",
+          },
+          {
+            en: "When Koichi and Viet got in, the taxi quietly started to move.",
+            ja: "\u30b3\u30a6\u30a4\u30c1\u3068\u30d3\u30a8\u30c8\u304c\u4e57\u308a\u8fbc\u3080\u3068\u3001\u305d\u306e\u30bf\u30af\u30b7\u30fc\u306f\u9759\u304b\u306b\u52d5\u304d\u51fa\u3057\u305f\u3002",
+          },
+        ],
+      },
+    },
+  },
+  {
+    word: "\u30ab\u30e0\u30d0\u30c3\u30af",
+    forvo: {
+      success: true,
+      error: null,
+      main_data: {
+        audio: [
+          {
+            url: "https://apifree.forvo.com/audio/3j2k2o3l3e2l2c3p3k1k2g323j1g3e3k1h3e2933381p1k3d342g3a1k2n1m341k3i3m33373n3e353l322p3c35263a1h2i2h1b1f1l2j1j1g3g2m3l2j2q3k2k293e35231m35392d3c2o3b2a28241b1l3d3i393c2734383n1t1t_2d2n3g2j282b1i213c311j3c2l2o27311m211k3g3p371t1t",
+            username: "strawberrybrown",
+          },
+        ],
+      },
+    },
+    jisho: {
+      success: true,
+      error: null,
+      main_data: {
+        results: [
+          {
+            slug: "\u30ab\u30e0\u30d0\u30c3\u30af",
+            is_common: true,
+            tags: [],
+            jlpt: ["jlpt-n1"],
+            japanese: [{ reading: "\u30ab\u30e0\u30d0\u30c3\u30af" }],
+            senses: [
+              {
+                english_definitions: ["comeback"],
+                parts_of_speech: ["Noun", "Suru verb", "Intransitive verb"],
+                links: [],
+                tags: [],
+                restrictions: [],
+                see_also: [],
+                antonyms: [],
+                source: [],
+                info: [],
+              },
+            ],
+            attribution: { jmdict: true, jmnedict: false, dbpedia: false },
+          },
+        ],
+        extra: [
+          {
+            slug: "51869cf2d5dda7b2c6074156",
+            japanese: [{ word: "\u30ab\u30e0\u30d0\u30c3\u30af\u8cde (NFL)" }],
+          },
+          {
+            slug: "5186a020d5dda7b2c608c40c",
+            japanese: [
+              {
+                word: "\u30ab\u30e0\u30d0\u30c3\u30af\u8cde (\u65e5\u672c\u30d7\u30ed\u91ce\u7403)",
+              },
+            ],
+          },
+          {
+            slug: "51869dc2d5dda7b2c607a433",
+            japanese: [{ word: "\u30ab\u30e0\u30d0\u30c3\u30af\u8cde (MLB)" }],
+          },
+        ],
+      },
+    },
+    ojad: {
+      success: true,
+      error: null,
+      main_data: {
+        accent: [
+          "\u304b\u3080\u3070' \u3063\u304f",
+          "\u304b' \u3080\u3070\u3063\u304f",
+        ],
+      },
+    },
+    suzuki: {
+      success: true,
+      error: null,
+      main_data: { accent: ["\u30ab\u30e0\u30d0' \u30c3\u30af"] },
+    },
+    wadoku: {
+      success: true,
+      error: null,
+      main_data: {
+        accent: [
+          "\u304b\u3080\u3070' \u3063\u304f",
+          "\u304b' \u3080\u3070\u3063\u304f",
+        ],
+      },
+    },
+    wanikani: {
+      success: true,
+      error: null,
+      main_data: { audio: [], sentences: [] },
+    },
+  },
+];
+
+export default testWordList;
