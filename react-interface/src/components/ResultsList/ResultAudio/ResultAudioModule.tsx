052616cd65664432687050172c28badf644a5804
Ben Leong <benleong0@gmail.com>
2021-12-28 00:24:08 +0000

    move to row and aurdio

--- react-interface/src/components/ResultsList/ResultAudio/ResultAudioModule.tsx
+++ react-interface/src/components/ResultsList/ResultAudio/ResultAudioModule.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ResultAudioRow from './ResultAudioRow';
 
 import ResultAudioRowData from '../../../types/ResultAudioRowData';
 
@@ -13,11 +14,7 @@ const ResultAudioModule: React.FC<ResultAudioModuleProps> = ({ moduleTitle, audi
         <div className="result-audio-module">
             <div className="audio-module-title">{ moduleTitle }</div>
             {audioData.map(audio =>
-                <div className="result-audio">
-                    <img src={ audio.url } alt="audio player icon" className="audio-player" />
-                    <div className="result-audio-speaker">{ audio.speaker }</div>
-                    <div className="result-audio-subtitle">{ audio.subtitle }</div>
-                </div>
+                <ResultAudioRow audioData={audio}/>
             )}
         </div>
     );
---
+++ react-interface/src/components/ResultsList/ResultAudio/ResultAudioRow.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+
+import ResultAudioRowData from '../../../types/ResultAudioRowData';
+
+import IconPlayButton from '../../../assets/icons/icon_play_button.svg'
+import IconStopButton from '../../../assets/icons/icon_stop_button.svg'
+
+
+interface ResultAudioRowProps {
+    audioData: ResultAudioRowData;
+}
+
+const ResultAudioRow: React.FC<ResultAudioRowProps> = ({ audioData }) => {
+    const [audio] = useState(new Audio(audioData.url));
+    const [playing, setPlaying] = useState(false);
+
+    const toggle = () => setPlaying(!playing);
+
+    useEffect(() => {
+        playing ? audio.play() : audio.pause();
+      },
+      [playing]
+    );
+
+    useEffect(() => {
+      audio.addEventListener('ended', () => setPlaying(false));
+      return () => {
+        audio.removeEventListener('ended', () => setPlaying(false));
+      };
+    }, []);
+
+    return (
+        <div className="result-audio">
+            <img
+                src={ playing ? IconStopButton : IconPlayButton }
+                alt="audio player icon"
+                className="audio-player"
+                onClick={toggle}
+            />
+            <div className="result-audio-speaker">{ audioData.speaker }</div>
+            <div className="result-audio-subtitle">{ audioData.subtitle }</div>
+        </div>
+     );
+}
+
+export default ResultAudioRow;
