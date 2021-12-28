7aedc56b7eb57d6f9bf0bac7025832e10db58994
Ben Leong <benleong0@gmail.com>
2021-12-28 16:10:14 +0000

    expand logic

--- react-interface/src/components/ResultsList/ResultAudio/ResultAudioModule.tsx
+++ react-interface/src/components/ResultsList/ResultAudio/ResultAudioModule.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ResultAudioRow from './ResultAudioRow';
 
 import ResultAudioRowData from '../../../types/ResultAudioRowData';
+
+import IconPlus from '../../../assets/icons/icon_plus.svg'
+import IconMinus from '../../../assets/icons/icon_minus.svg'
 
 
 interface ResultAudioModuleProps {
@@ -10,12 +13,23 @@ interface ResultAudioModuleProps {
 }
 
 const ResultAudioModule: React.FC<ResultAudioModuleProps> = ({ moduleTitle, audioData}) => {
+    const [isExpanded, setIsExpanded] = useState<boolean>(false);
+    const toggleIsExpanded = () => setIsExpanded(!isExpanded);
+
     return (
         <div className="result-audio-module">
             <div className="audio-module-title">{ moduleTitle }</div>
-            {audioData.map(audio =>
-                <ResultAudioRow key={audio.speaker} audioData={audio}/>
+            {audioData.map((audio, rowIndex) =>
+                <ResultAudioRow key={audio.speaker} rowIndex={rowIndex} audioData={audio} isExpanded={isExpanded}/>
             )}
+            {audioData.length > 2 &&
+                <div className="audio-expand-toggle" onClick={toggleIsExpanded}>
+                    <img
+                        src={isExpanded ? IconMinus : IconPlus}
+                        alt="button to toggle expanded list of audio files"
+                    />
+                </div>
+            }
         </div>
     );
 }
--- react-interface/src/components/ResultsList/ResultAudio/ResultAudioRow.tsx
+++ react-interface/src/components/ResultsList/ResultAudio/ResultAudioRow.tsx
@@ -8,30 +8,33 @@ import IconStopButton from '../../../assets/icons/icon_stop_button.svg'
 
 
 interface ResultAudioRowProps {
+    rowIndex: number;
     audioData: ResultAudioRowData;
+    isExpanded: boolean;
 }
 
-const ResultAudioRow: React.FC<ResultAudioRowProps> = ({ audioData }) => {
+const ResultAudioRow: React.FC<ResultAudioRowProps> = ({ rowIndex, audioData, isExpanded }) => {
     const [audio] = useState(new Audio(audioData.url));
     const [playing, setPlaying] = useState(false);
 
     const togglePlaying = () => setPlaying(!playing);
 
     useEffect(() => {
-        audio.currentTime = 0;
-        playing ? audio.play() : audio.pause();
-      },
-      [playing]
+            audio.currentTime = 0;
+            playing ? audio.play() : audio.pause();
+        },
+        [playing]
     );
 
     useEffect(() => {
-      audio.addEventListener('ended', () => setPlaying(false));
-      return () => {
-        audio.removeEventListener('ended', () => setPlaying(false));
-      };
+        audio.addEventListener('ended', () => setPlaying(false));
+        return () => {
+            audio.removeEventListener('ended', () => setPlaying(false));
+        };
     }, []);
 
     return (
+        (isExpanded || rowIndex < 2) ?
         <div className="result-audio-row flex-row">
             <img
                 src={ playing ? IconStopButton : IconPlayButton }
@@ -41,7 +44,8 @@ const ResultAudioRow: React.FC<ResultAudioRowProps> = ({ audioData }) => {
             />
             <div className="result-audio-speaker">{ audioData.speaker }</div>
             <div className="result-audio-subtitle">{ audioData.subtitle || "" }</div>
-        </div>
+        </div> :
+        <></>
      );
 }
 
