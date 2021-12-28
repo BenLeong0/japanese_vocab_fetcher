5eb8f011c1e5c50b93f56928885a37595d610cbc
Ben Leong <benleong0@gmail.com>
2021-12-28 01:16:09 +0000

    jppod101

--- react-interface/src/components/ResultsList/ResultAudio/ResultAudio.tsx
+++ react-interface/src/components/ResultsList/ResultAudio/ResultAudio.tsx
@@ -23,14 +23,20 @@ const ResultAudio: React.FC<ResultAudioProps> = ({ data }) => {
         speaker: audio.username,
         subtitle: null,
     }))
+    const japanesePodData: ResultAudioRowData[] = [{
+        url: "https://audiostock-public-files.s3.ap-northeast-1.amazonaws.com/sample-files/demo_34d52dc1ec6ba4736f37c24458a2a7812e9b56f6.mp3",
+        speaker: "Coming soon!",
+        subtitle: null
+    }]
 
     return (
         <div className="result-audio">
             <div className="left-col-title">
                 Audio
             </div>
-            <ResultAudioModule moduleTitle="Wanikani" audioData={wanikaniData} />
-            <ResultAudioModule moduleTitle="Forvo" audioData={forvoData} />
+            {wanikaniData.length > 0 && <ResultAudioModule moduleTitle="Wanikani" audioData={wanikaniData} />}
+            {forvoData.length > 0 && <ResultAudioModule moduleTitle="Forvo" audioData={forvoData} />}
+            <ResultAudioModule moduleTitle="JapanesePod101" audioData={japanesePodData} />
         </div>
      );
 }
