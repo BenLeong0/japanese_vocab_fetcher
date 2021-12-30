046f87a7bdbc924e04276f162c3b33a612b29e74
Ben Leong <benleong0@gmail.com>
2021-12-30 22:48:53 +0000

    initialise

---
+++ react-interface/src/components/ResultsList/ResultSentences/ResultSentences.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import FullResponseItem from '../../../types/FullResponseItem';
+
+import './ResultSentences.css'
+
+
+interface ResultSentencesProps {
+    data: FullResponseItem
+}
+
+const ResultSentences: React.FC<ResultSentencesProps> = () => {
+    return ( <></> );
+}
+
+export default ResultSentences;
