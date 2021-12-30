a28e5002019c38e99d12d9421241bc4e2a989e51
Ben Leong <benleong0@gmail.com>
2021-12-30 21:39:38 +0000

    fns

--- react-interface/src/components/ResultsList/ResultDefinitions/ResultDefinitionSense.tsx
+++ react-interface/src/components/ResultsList/ResultDefinitions/ResultDefinitionSense.tsx
@@ -9,15 +9,21 @@ interface ResultDefinitionSenseProps {
 }
 
 const ResultDefinitionSense: React.FC<ResultDefinitionSenseProps> = ({ ordinality, sense }) => {
+
+    const dfn = ordinality + '. ' + sense.english_definitions.join("; ");
+
+    const getWordType = () => {
+        let wordType = sense.parts_of_speech.join(", ");
+        if (sense.tags.length > 0) {
+            wordType += " (" + sense.tags.join(", ") + ")";
+        }
+        return wordType
+    }
+
     return (
         <div className="result-definition-sense flex-col">
-            <div className="sense-part-of-speech">{
-                sense.parts_of_speech.join(", ") +
-                (sense.tags.length > 0 ? " (" + sense.tags.join(", ") + ")" : "")
-            }</div>
-            <div className="sense-english-definition">
-                { ordinality }. { sense.english_definitions.join("; ") }
-            </div>
+            <div className="sense-part-of-speech">{ getWordType() }</div>
+            <div className="sense-english-definition">{ dfn }</div>
         </div>
     );
 }
