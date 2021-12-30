c22faf7b3b24bf749cef3971aad2236398a300af
Ben Leong <benleong0@gmail.com>
2021-12-30 21:31:28 +0000

    spacing and scolons

--- react-interface/src/components/ResultsList/ResultReadings/ResultReadings.tsx
+++ react-interface/src/components/ResultsList/ResultReadings/ResultReadings.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import FullResponseItem from '../../../types/FullResponseItem';
 
-import './ResultReadings.css'
+import './ResultReadings.css';
 import ResultReadingsRow from './ResultReadingsRow';
 
 
@@ -12,17 +12,17 @@ interface ResultReadingsProps {
 
 const ResultReadings: React.FC<ResultReadingsProps> = ({ data }) => {
     const sitesAndReadings = [
-        { title: "OJAD", readings: data.ojad.main_data.accent},
-        { title: "Wadoku", readings: data.wadoku.main_data.accent},
-        { title: "Suzuki", readings: data.suzuki.main_data.accent},
-    ]
+        { title: "OJAD", readings: data.ojad.main_data.accent },
+        { title: "Wadoku", readings: data.wadoku.main_data.accent },
+        { title: "Suzuki", readings: data.suzuki.main_data.accent },
+    ];
 
     return (
         <div className="result-readings">
             <div className="left-col-title">
                 Readings
             </div>
-            {sitesAndReadings.map(({title, readings}) =>
+            {sitesAndReadings.map(({ title, readings }) =>
                 <ResultReadingsRow key={title} title={title} readings={readings} />
             )}
         </div>
