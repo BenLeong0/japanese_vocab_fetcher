27eac142428426cec2463966fc685a8e2cf31844
Ben Leong <benleong0@gmail.com>
2022-01-04 22:59:58 +0000

    display related words

--- react-interface/src/components/ResultsList/ResultRelatedWords/ResultRelatedWords.tsx
+++ react-interface/src/components/ResultsList/ResultRelatedWords/ResultRelatedWords.tsx
@@ -11,7 +11,12 @@ interface ResultRelatedWordsProps {
 }
 
 const ResultRelatedWords: React.FC<ResultRelatedWordsProps> = ({ data }) => {
+    const displayRelatedWords = (): boolean => {
+        return data.jisho.main_data.extra.length > 0;
+    }
+
     return (
+        displayRelatedWords() ?
         <div className="result-related-words vertical-separation-small">
             <div className="left-col-title">
                 Related words
@@ -21,7 +26,8 @@ const ResultRelatedWords: React.FC<ResultRelatedWordsProps> = ({ data }) => {
                     <ResultRelatedWord key={result.slug} relatedWord={result}/>
                 )}
             </div>
-        </div>
+        </div> :
+        <></>
      );
 }
 
