5c1e244ed30bd06259cef4dba6fdab556b233d68
Ben Leong <benleong0@gmail.com>
2022-01-16 22:01:33 +0000

    copy reading to clipboard on click

--- react-interface/src/components/ResultsList/ResultReadings/ResultReadingsRow.tsx
+++ react-interface/src/components/ResultsList/ResultReadings/ResultReadingsRow.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import UtilsService from '../../core/UtilsService';
+
 
 interface ResultReadingsRowProps {
     title: string;
@@ -7,12 +9,22 @@ interface ResultReadingsRowProps {
 }
 
 const ResultReadingsRow: React.FC<ResultReadingsRowProps> = ({ title, readings }) => {
+    const utilsService = new UtilsService();
+    const copyReadingToClipboard = (reading: string) => {
+        utilsService.copyStringToClipboard(reading);
+    }
+
     return (
         <div className="readings-row">
             <div className="readings-row-left unselectable">{ title }:</div>
             <div className="readings-row-right">
                 {readings.map((reading, index) =>
-                    <div key={index} className="reading">{ reading }</div>
+                    <div
+                        key={index}
+                        className="reading unselectable"
+                        onClick={() => {copyReadingToClipboard(reading)}}
+                        title="Copy to clipboard"
+                    >{ reading }</div>
                 )}
             </div>
         </div>
