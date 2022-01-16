8ec6a1ebd02cc13911de1f4b9eed9ce1374c4719
Ben Leong <benleong0@gmail.com>
2022-01-16 21:54:22 +0000

    copy functionality

--- react-interface/src/components/ResultsList/ResultSentences/ResultSentence.tsx
+++ react-interface/src/components/ResultsList/ResultSentences/ResultSentence.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import Sentence from '../../../types/Sentence';
 
+import UtilsService from '../../core/UtilsService';
+
+import { ReactComponent as IconCopy } from '../../../assets/icons/icon_copy.svg';
+
 
 export interface ResultSentenceProps {
     sentence: Sentence;
@@ -9,11 +13,22 @@ export interface ResultSentenceProps {
 }
 
 const ResultSentence: React.FC<ResultSentenceProps> = ({ sentence, source }) => {
+    const utilsService = new UtilsService();
+    const copySentenceToClipboard = () => {
+        utilsService.copyStringToClipboard(sentence.ja + '\t' + sentence.en);
+    }
+
     return (
         <div className="result-sentence flex-col">
             <div className="result-sentence-top flex-row">
                 <div className="result-sentence-ja">{ sentence.ja }</div>
-                <div className="result-sentence-source">{ `- ${source}` }</div>
+                <div className="result-sentence-extra flex-row">
+                    <div className="result-sentence-source">{ `- ${source}` }</div>
+                    <IconCopy
+                        className="result-sentence-copy"
+                        onClick={copySentenceToClipboard}
+                    />
+                </div>
             </div>
             <div className="result-sentence-en">{ sentence.en }</div>
         </div>
