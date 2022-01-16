869f8322a432e550c33dc54770fd7872055fe5bb
Ben Leong <benleong0@gmail.com>
2022-01-16 21:01:57 +0000

    img and format title

--- react-interface/src/components/ResultsList/ResultTitle/ResultTitle.tsx
+++ react-interface/src/components/ResultsList/ResultTitle/ResultTitle.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './ResultTitle.css';
+import IconCopy from '../../../assets/icons/icon_copy.svg'
 
 
 interface ResultTitleProps {
@@ -12,13 +13,20 @@ interface ResultTitleProps {
 const ResultTitle: React.FC<ResultTitleProps> = ({ children, isExpanded, toggleIsExpanded }) => {
 
     return (
-        <div className="result-title">
+        <div className="result-title flex-row space-between">
             <div className="result-title-text mplus">{ children }</div>
-            <div
-                className="title-collapser unselectable"
-                onClick={toggleIsExpanded}
-            >
-                { isExpanded ? "collapse" : "expand" }
+            <div className="result-title-buttons flex-col space-between">
+                <img
+                    className="result-title-copy unselectable"
+                    src={IconCopy}
+                    alt="button to copy sample row to clipboard"
+                />
+                <div
+                    className="result-title-collapser unselectable"
+                    onClick={toggleIsExpanded}
+                >
+                    { isExpanded ? "collapse" : "expand" }
+                </div>
             </div>
         </div>
     );
