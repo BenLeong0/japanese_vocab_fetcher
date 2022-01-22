bfaa51a2d4cf19eebb0d6b5f090b246516cc2e6c
Ben Leong <benleong0@gmail.com>
2022-01-22 18:15:20 +0000

    docstrings

--- python_backend/testing/test_tangorin.py
+++ python_backend/testing/test_tangorin.py
@@ -30,8 +30,8 @@ class FakeResponse:
 def test_main(monkeypatch, test_dict: FullTestDict):
     """
     - GIVEN a list of words
-    - WHEN the accent dict is generated
-    - THEN check all the tangorin info is correct and complete
+    - WHEN the audio dict is generated
+    - THEN check all the japanesepod info is correct and complete
     """
     word_list = convert_list_of_str_to_kaki(test_dict['input'])
     sections = test_dict['tangorin']['expected_sections']
@@ -54,7 +54,7 @@ def test_main(monkeypatch, test_dict: FullTestDict):
 def test_empty_input():
     """
     - GIVEN an empty input
-    - WHEN an accent dictionary is generated
+    - WHEN an audio dictionary is generated
     - THEN check it returns and empty dict
     """
     assert tangorin.main([]) == {}
