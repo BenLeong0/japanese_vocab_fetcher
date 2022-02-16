4b95d95499bbd5103ec03dda4f8301db509aa811
Ben Leong <benleong0@gmail.com>
2022-02-16 23:11:58 +0000

    test file

---
+++ python_backend/testing/test_tatoeba.py
@@ -0,0 +1,43 @@
+import pytest   # type: ignore
+
+from modules import tatoeba
+from testing.dict_typing import FullTestDict
+from testing.dicts import TEST_DICTS
+from utils import convert_list_of_str_to_kaki
+
+
+# For each test, try with every dict in TEST_DICTS
+@pytest.fixture(params=TEST_DICTS, ids=lambda d:d['id'])
+def test_dict(request):
+    return request.param
+
+
+class FakeResponse:
+    def __init__(self, text, status_code=200, error=""):
+        self.text = text
+        self.status_code = status_code
+
+
+#####################
+## TESTS  ###########
+#####################
+
+def test_main(test_dict: FullTestDict):
+    """
+    - GIVEN a list of words
+    - WHEN the accent dict is generated
+    - THEN check all the tatoeba info is correct and complete
+    """
+    word_list = convert_list_of_str_to_kaki(test_dict['input'])
+    expected_output = test_dict['tatoeba']['expected_output']
+
+    assert tatoeba.main(word_list) == expected_output
+
+
+def test_empty_input():
+    """
+    - GIVEN an empty input
+    - WHEN an accent dictionary is generated
+    - THEN check it returns and empty dict
+    """
+    assert tatoeba.main([]) == {}
